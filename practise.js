49937a346be8fd8dddba831f01e5bb5436137904
Muhammad Aqib khan <Khansons0543@gmail.com>
2023-10-01 09:34:50 +0500

    I have created atm mashion

---
+++ practise.js
@@ -0,0 +1,30 @@
+const Accounts = [
+    {
+        AccNO: "223344G",
+        Pin: 'ABC3',
+        Balance: '100000',
+    },
+    {
+        AccNO: "223",
+        Pin: 'DBC3',
+        Balance: '90000',
+    },
+    { AccNO: "234G",
+        Pin: 'DDC3',
+        Balance: '80000', },
+    {
+        AccNO: "223G",
+        Pin: 'CCC3',
+        Balance: '85000',
+    },
+];
+let currentUser = null;
+async function display() {
+    const choices = [
+        "login",
+        "Exit"
+    ];
+    while (true) {
+    }
+}
+export {};
---
+++ practise.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+    const Accounts=[       
+    { 
+       AccNO:"223344G",
+       Pin:'ABC3',
+      Balance:'100000',
+      
+    },
+    {
+      AccNO:"223",
+       Pin:'DBC3',
+      Balance:'90000',
+    },
+    {AccNO:"234G",
+    Pin:'DDC3',
+           Balance:'80000',},
+    {
+      AccNO:"223G",
+       Pin:'CCC3',
+      Balance:'85000',
+    },
+   
+]
+
+ let currentUser:any=null;
+async function display() {
+     const choices=[
+      "login",
+      "Exit"
+     ];
+     while (true) {
+      
+     }  
+  
+
+}
+  
+
+
